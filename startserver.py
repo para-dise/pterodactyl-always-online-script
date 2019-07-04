8bc66083d79164c0afb5af626091ad5527924995
paradise <xkcocco@yahoo.com>
2019-07-04 12:42:26 +0100

    Update startserver.py

--- startserver.py
+++ startserver.py
@@ -1,13 +1,13 @@
 import requests
-import time
+from time import sleep as slp
 
 headers = {
-        "Authorization": "Bearer YOUR_API_TOKEN",
+        "Authorization": "Bearer YOUR_API_KEY", #get your api key from https://panel.your.host/account/api
         "Content-Type": "application/json",
         "Accept": "Application/vnd.pterodactyl.v1+json"
 }
 servers = ["0aa00000", "0aa00aaa"] # a list of server IDs, eg 6cd06969
-baseurl = "https://panel.your.host" #configure this to be a pterodactyl base url. eg "https://panel.freemc.host" don't include a slash at the end
+baseurl = "https://panel.freemc.host" #configure this to be a pterodactyl base url. eg "https://panel.freemc.host" don't include a slash at the end
 
 while True:
     for server in servers:
@@ -16,4 +16,4 @@ while True:
             print("\33[33m[Notice] \33[37m> \33[32mServer successfully started.")
         else:
             print("\33[33m[Notice] \33[37m> \33[31mGot a different response. [%s] with [%s]" % (r.status_code, r.text))
-    time.sleep(120)
+    slp(120) #amount of seconds to sleep
